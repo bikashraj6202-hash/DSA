26225001f9e4feb9301dec910c1d2acf0e16151e
bikashraj6202-hash <bikashraj6202@gmail.com>
2025-08-10 09:01:00 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (67.98%) - LeetSync

---
+++ 46-permutations/permutations.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+
+void prems(int i,vector<vector<int>>&res,vector<int>&nums){
+    // base case 
+    if(i==nums.size()){
+        res.push_back(nums);
+    }
+
+    for(int j=i;j<nums.size();j++){
+        swap(nums[i],nums[j]);
+        prems(i+1,res,nums);
+        swap(nums[i],nums[j]);
+    }
+}
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>>res;
+        prems(0,res,nums);
+        return res;
+
+
+    }
+};
