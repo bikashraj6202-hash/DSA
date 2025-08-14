82c8488f10609a5392e6857b80f63decf40e74db
bikashraj6202-hash <bikashraj6202@gmail.com>
2025-08-15 00:51:56 +0530

    Time: 0 ms (100.00%) | Memory: 23.6 MB (72.26%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
@@ -11,22 +11,32 @@
  */
 class Solution {
 public:
-int height(TreeNode* root){
-    if(root==NULL){
-        return 0;
-    }
-    int left=height(root->left);
-    int right=height(root->right);
-    return max(left,right)+1 ; // include node here
+pair<int,int>diafast(TreeNode *node){
+    // base case
+    if(node==NULL){
+        pair<int,int>p(0,0);
+        return p;
 }
-    int diameterOfBinaryTree(TreeNode* root) {
-        if(root==NULL){
-            return 0;
-        }
-        int op1=diameterOfBinaryTree(root->left); // left subtree kahi answer left subtree se na ban ho
-        int op2=diameterOfBinaryTree(root->right); // right subtree me jiske ans
-        int op3=height(root->left)+height(root->right);
-        return max(op3,max(op1,op2));
-        
-    }
+pair<int,int>left=diafast(node->left);
+pair<int,int>right=diafast(node->right);
+
+int op1=left.first;
+int op2=right.first;
+int op3=left.second+right.second;
+
+pair<int,int>ans;
+ans.first=max(op1,max(op2,op3));
+ans.second=max(left.second,right.second)+1;
+return ans;
+}
+
+
+int diameterOfBinaryTree(TreeNode *root){
+    return diafast(root).first;
+}   
+
+
+
+
+
 };
