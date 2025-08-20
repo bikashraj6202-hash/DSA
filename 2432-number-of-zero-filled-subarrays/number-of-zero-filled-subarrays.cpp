38e0fea17e7990c5c6d96549a3c9ffcb31bea673
bikashraj6202-hash <bikashraj6202@gmail.com>
2025-08-20 21:41:40 +0530

    Time: 0 ms (100.00%) | Memory: 111.3 MB (40.81%) - LeetSync

--- 2432-number-of-zero-filled-subarrays/number-of-zero-filled-subarrays.cpp
+++ 2432-number-of-zero-filled-subarrays/number-of-zero-filled-subarrays.cpp
@@ -1,23 +1,18 @@
 class Solution {
 public:
     long long zeroFilledSubarray(vector<int>& arr) {
-        vector<int>ans;
+        long long sum=0;
         int n=arr.size();
         int count=0;
         for(int i=0;i<n;i++){
             if(arr[i]==0){
                 count++; 
-                ans.push_back(count);
+             sum+=count;
             }
             else{
                 count=0;
             } 
         }
-        long long sum=0;
-    int p=ans.size();
-    for(int i=0;i<p;i++){
-        sum+=ans[i];
-    }
-return sum;
+      return sum;
     }
 };
