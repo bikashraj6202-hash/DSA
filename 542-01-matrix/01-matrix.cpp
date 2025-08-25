75830280cef0e5bab9b25558e4bf6f9a11bc1013
bikashraj6202-hash <bikashraj6202@gmail.com>
2025-08-26 02:06:45 +0530

    Time: 28 ms (26.27%) | Memory: 38.9 MB (34.49%) - LeetSync

---
+++ 542-01-matrix/01-matrix.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int n=mat.size();
+        int m=mat[0].size();
+        vector<vector<int>>vis(n,vector<int>(m,0));
+        vector<vector<int>>dis(n,vector<int>(m,0));
+        queue<pair<pair<int,int>,int> >q; // (row,col),distance;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(mat[i][j]==0){
+                    q.push({{i,j},0});
+                    vis[i][j]=1;
+                }
+            }
+        }
+        while(!q.empty()){
+            int r=q.front().first.first;
+            int c=q.front().first.second;
+            int step=q.front().second;
+            q.pop();
+            dis[r][c]=step;
+            int delrow[]={-1,0,1,0};
+            int delcol[]={0,1,0,-1};
+            for(int i=0;i<4;i++){
+                int nrow=r+delrow[i];
+                int ncol=c+delcol[i];
+                if(nrow>=0 && nrow<n && ncol<m && ncol>=0 
+                && vis[nrow][ncol]==0){
+                    vis[nrow][ncol]=1;
+                    q.push({{nrow,ncol},step+1});    
+                }
+            }
+        }
+        return dis;
+    }
+};
