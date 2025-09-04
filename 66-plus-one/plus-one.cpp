a4deb6df20951d50e0d21c0b8daa862803d435c9
bikashraj6202-hash <bikashraj6202@gmail.com>
2025-09-05 01:20:17 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (7.03%) - LeetSync

---
+++ 66-plus-one/plus-one.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int n = digits.size();
+
+        for (int i = n - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i] += 1;
+                return digits;  // no carry, done
+            }
+            digits[i] = 0; // carry over
+        }
+
+        // If we finished loop, it means all digits were 9
+        digits.insert(digits.begin(), 1);
+        return digits;
+    }
+};
