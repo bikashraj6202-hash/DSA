e5104c0805d8ab570ee2d9ff6d8894642ac4822d
bikashraj6202-hash <bikashraj6202@gmail.com>
2025-09-06 23:40:21 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (24.16%) - LeetSync

--- 507-perfect-number/perfect-number.cpp
+++ 507-perfect-number/perfect-number.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     bool checkPerfectNumber(int num) {
-        int sum=0;
-        for(int i=1;i<=(num/2);i++){
-            if(num%i==0){
-                sum=sum+i;
+        if(num <= 1) return false;  // 1 is not a perfect number
+
+        int sum = 1; // 1 is always a divisor
+        for(int i=2; i*i<=num; i++){
+            if(num % i == 0){
+                sum += i;
+                if(i != num/i) sum += num/i;  // add the complementary divisor
             }
         }
-        if(sum==num)return true;
-        else return false;
+        return sum == num;
     }
 };
