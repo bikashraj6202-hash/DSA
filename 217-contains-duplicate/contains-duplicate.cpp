e5dae277e5b4d537051d7b85b9b33f5709bcb2d8
bikashraj6202-hash <bikashraj6202@gmail.com>
2025-09-07 23:19:52 +0530

    Time: 59 ms (27.99%) | Memory: 93 MB (15.89%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int,int>m;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            m[nums[i]]++;
+        }
+        int count=0;
+        for(int i=0;i<n;i++){
+            if(m[nums[i]]>1){
+                count=1;
+                return true;
+            }
+        }
+        return false;
+    }
+};
