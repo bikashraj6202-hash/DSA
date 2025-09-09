9cfc778273cd344cd1b4419ac2ca0589f1eff536
bikashraj6202-hash <bikashraj6202@gmail.com>
2025-09-09 22:20:36 +0530

    Time: 2 ms (40.03%) | Memory: 15.4 MB (93.33%) - LeetSync

--- 2277-count-equal-and-divisible-pairs-in-an-array/count-equal-and-divisible-pairs-in-an-array.cpp
+++ 2277-count-equal-and-divisible-pairs-in-an-array/count-equal-and-divisible-pairs-in-an-array.cpp
@@ -5,7 +5,7 @@ public:
         int n=nums.size();
         for(int i=0;i<n;i++){
             for(int j=i+1;j<n;j++){
-                if(nums[i]==nums[j] && i<j && (i*j)%k==0){
+                if(nums[i]==nums[j]  && (i*j)%k==0){
                     count++;
                 }
             }
