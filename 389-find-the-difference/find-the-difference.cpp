5d6efb26dc575b33510578b6d64d234bae74f192
bikashraj6202-hash <bikashraj6202@gmail.com>
2025-09-16 20:16:37 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (95.08%) - LeetSync

--- 389-find-the-difference/find-the-difference.cpp
+++ 389-find-the-difference/find-the-difference.cpp
@@ -1,15 +1,27 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        int i=0;
-        for(char &ch:s){
-            if(ch!=t[i]){
-                return t[i];
-            }
-            i++;
+        // sort(s.begin(),s.end());
+        // sort(t.begin(),t.end());
+        // int i=0;
+        // for(char &ch:s){
+        //     if(ch!=t[i]){
+        //         return t[i];
+        //     }
+        //     i++;
+        // }
+        // return t[i];
+
+
+        //using second method
+
+        int xorsum=0;
+        for(int i=0;i<s.size();i++){
+            xorsum=xorsum^s[i];
         }
-        return t[i];
+        for(int i=0;i<t.size();i++){
+            xorsum=xorsum^t[i];
+        }
+        return char(xorsum);
     }
 };
