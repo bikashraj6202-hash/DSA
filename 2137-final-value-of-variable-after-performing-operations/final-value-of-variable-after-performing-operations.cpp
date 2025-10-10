555ceb8e864ebf05b33fe7b54f61c8bd7ccacaf0
bikashraj6202-hash <bikashraj6202@gmail.com>
2025-10-10 23:51:56 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (43.61%) - LeetSync

---
+++ 2137-final-value-of-variable-after-performing-operations/final-value-of-variable-after-performing-operations.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int finalValueAfterOperations(vector<string>& operations) {
+        int count=0;
+        int n=operations.size();
+        for(int i=0;i<n;i++){
+            if( (operations[i]=="--X")||(operations[i]=="X--") ){
+                count--;
+            }
+             if( (operations[i]=="++X")||(operations[i]=="X++") ){
+                count++;
+            }
+        }
+        return count;
+    }
+};
