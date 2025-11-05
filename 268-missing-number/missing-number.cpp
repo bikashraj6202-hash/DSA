24415574dc72330b9fb07446c71705cb37c67598
bikashraj6202-hash <bikashraj6202@gmail.com>
2025-11-05 22:30:02 +0530

    Time: 9 ms (12.61%) | Memory: 21.8 MB (69.89%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -1,13 +1,21 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int n=nums.size();
-        sort(nums.begin(),nums.end());
-        for(int i=0;i<n;i++){
-            if(nums[i]!=i){
-                return i;
-            }
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        int i = 0, j = n - 1;
+
+        // If 0 is missing
+        if (nums[0] != 0) return 0;
+
+        while (i <= j) {
+            int mid = (i + j) / 2;
+            if (nums[mid] == mid)
+                i = mid + 1;   // search right
+            else
+                j = mid - 1;   // search left
         }
-        return n;
+
+        return i;  // missing number is the first index where nums[index] != index
     }
 };
