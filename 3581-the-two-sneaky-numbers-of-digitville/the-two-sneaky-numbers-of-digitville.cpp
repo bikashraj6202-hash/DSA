313849851534b269a45f27920f33949cfc28825f
bikashraj6202-hash <bikashraj6202@gmail.com>
2026-01-26 00:52:03 +0530

    Time: 4 ms (21.74%) | Memory: 26.2 MB (47.21%) - LeetSync

--- 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.cpp
+++ 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     vector<int> getSneakyNumbers(vector<int>& nums) {
         int n=nums.size();
-        unordered_map<int,int>mp;
+        vector<int>freq(n-2,-1);
         vector<int>ans;
         for(int i=0;i<n;i++){
-            if((mp[nums[i]]++)==2){
+            if((++freq[nums[i]])==2){
                 ans.push_back(nums[i]);
             }
             else{
-                mp[nums[i]]++;
+                freq[nums[i]]++;
             }
         }
         return ans;
