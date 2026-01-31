af4143faa7ac4f47126a584ffcdf1160ac3c99e7
bikashraj6202-hash <bikashraj6202@gmail.com>
2026-01-31 10:56:16 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (44.65%) - LeetSync

---
+++ 165-compare-version-numbers/compare-version-numbers.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int compareVersion(string v1, string v2) {
+        int i=0;
+        int j=0;
+        int n1=v1.size();
+        int n2=v2.size(); 
+        while(i<n1 || j<n2){
+            int num1=0;
+            int num2=0;
+      while( i<n1 && v1[i]!='.'){
+            num1=num1*10+ (v1[i]-'0');
+            i++;
+        }
+        while(j<n2 && v2[j]!='.'){
+            num2=num2*10+ (v2[j]-'0');
+            j++;
+        }
+        if(num1>num2) return 1;
+        else if(num2>num1) return -1;
+        i++;
+        j++;
+    }
+    return 0;
+    }
+};
