6e11b0553e24ec13167c1335d2f5426afb3cbcbe
bikashraj6202-hash <bikashraj6202@gmail.com>
2026-02-04 23:25:28 +0530

    Time: 0 ms (100.00%) | Memory: 14.1 MB (57.85%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -2,11 +2,15 @@ class Solution {
 public:
     
     void solve(int idx,vector<int>&arr, vector<vector<int>>&ans,vector<int>&ds,int target){
-        if(idx==arr.size()){
-            if(target==0){
+         if(target==0){
                 ans.push_back(ds);
-            
+                return;
             }
+        if(idx==arr.size()){
+            // if(target==0){
+            //     ans.push_back(ds);
+            
+            // }
                 return ;
         }
 
