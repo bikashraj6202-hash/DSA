c84cfb74b02bbd0151cfe2054610b5ab9fa9d827
bikashraj6202-hash <bikashraj6202@gmail.com>
2026-02-04 23:21:00 +0530

    Time: 5 ms (31.12%) | Memory: 14.1 MB (57.85%) - LeetSync

---
+++ 39-combination-sum/combination-sum.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    
+    void solve(int idx,vector<int>&arr, vector<vector<int>>&ans,vector<int>&ds,int target){
+        if(idx==arr.size()){
+            if(target==0){
+                ans.push_back(ds);
+            
+            }
+                return ;
+        }
+
+        // pick the elemnet 
+        if(arr[idx]<=target){
+            ds.push_back(arr[idx]);
+            solve(idx,arr,ans,ds,target-arr[idx]);
+            ds.pop_back();
+        }
+
+        solve(idx+1,arr,ans,ds,target);
+    }
+    
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+       vector<vector<int>>ans;
+       vector<int>ds;
+       solve(0,candidates,ans,ds,target);
+
+       return ans;
+    }
+};
