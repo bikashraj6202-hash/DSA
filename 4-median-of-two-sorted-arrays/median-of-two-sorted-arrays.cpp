ce1e8ee13ed9b51ff31ccdc4cbcc497997a2c974
bikashraj6202-hash <bikashraj6202@gmail.com>
2026-03-22 14:18:37 +0530

    Time: 57 ms (5.10%) | Memory: 96 MB (9.88%) - LeetSync

---
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int i=0;
+        int j=0;
+        vector<int>v;
+        while(i<nums1.size() && j<nums2.size()){
+            if(nums1[i]<=nums2[j]){
+                v.push_back(nums1[i]);
+                i++;
+            }
+            else{
+                v.push_back(nums2[j]);
+                j++;
+            }
+        }
+        while(i<nums1.size()){
+            v.push_back(nums1[i]);
+            i++;
+        }
+        while(j<nums2.size()){
+            v.push_back(nums2[j]);
+            j++;
+        }
+        for(int i=0;i<v.size();i++){
+            cout<<v[i];
+        }
+        int n=v.size();
+        if(n%2==0){
+            return (double)(v[n/2]+v[(n/2)-1])/2;
+        }
+        else{
+            return v[n/2];
+        }
+    }
+};
