1cc002a5b4acb0045ed3b269f4f8b64ad73b1bef
bikashraj6202-hash <bikashraj6202@gmail.com>
2026-06-30 11:00:47 +0530

    Time: 3 ms (19.14%) | Memory: 71.8 MB (51.96%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -1,23 +1,16 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        if(nums.size()==1)return nums[0];
-        int i=0;
-        int j=1;
-        int n=nums.size();
-        int sum=nums[0];
-        int maxsum=sum;
-        while(j<n){
-            if((nums[j]+sum)<nums[j]){
-                i=j;
-                sum=nums[j];
-            }
-            else{
-                sum=sum+nums[j];
-            }
-            j++;
-            maxsum=max(maxsum,sum);
+      int sum=0;
+      int mxsum=INT_MIN;
+      for(int i=0;i<nums.size();i++){
+        sum+=nums[i];
+          mxsum=max(mxsum,sum);
+        if(sum<0){
+            sum=0;
         }
-        return maxsum;
+
+      }
+      return mxsum;
     }
 };
