a745084a696c7e5e83ee0be6a004bb82fec2fedc
bikashraj6202-hash <bikashraj6202@gmail.com>
2026-07-02 09:36:13 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (97.17%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,31 +1,12 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int left=0;
-        int right=0;
-        vector<int>result;
-        while(left<m && right<n){
-            if(nums1[left]<=nums2[right]){
-                result.push_back(nums1[left]);
-                       left++;
-            }
-            else{
-                  result.push_back(nums2[right]); 
-                   right++;
-            }
-     
-          
-        }
-        while(left<m){
-              result.push_back(nums1[left]);
-              left++;
-        }
-        while(right<n){
-              result.push_back(nums2[right]);
-              right++;
-        }
-        for(int i=0;i<result.size();i++){
-            nums1[i]=result[i];
-        }
+        int p=m;
+       for(int i=0;i<n;i++){
+        nums1[p]=nums2[i];
+        p++;
+       }
+       sort(nums1.begin(),nums1.end());
+       return ;
     }
 };
