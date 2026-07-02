f6dde71dc55f3c0fd285cf024c87d312e259e05a
bikashraj6202-hash <bikashraj6202@gmail.com>
2026-07-02 10:04:35 +0530

    Time: 3 ms (89.15%) | Memory: 23.8 MB (59.42%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -1,14 +1,21 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        if(intervals.size()<=1)return intervals;
         sort(intervals.begin(),intervals.end());
-        vector<vector<int>>output;
-        output.push_back(intervals[0]);
-        for(int i=1;i<intervals.size();i++){
-            if(output.back()[1]>=intervals[i][0]) output.back()[1]=max(output.back()[1],intervals[i][1]);
-            else output.push_back(intervals[i]);
+       vector<vector<int>>ans;
+       ans.push_back(intervals[0]);
+       for(int i=1;i<intervals.size();i++){
+        if(intervals[i][0]<=ans.back().back() && intervals[i][1]>ans.back().back()){
+            ans.back().back()=intervals[i][1];
         }
-        return output;
+        else if(intervals[i][0]<=ans.back().back() && intervals[i][1]<=ans.back().back()){
+            continue;
+        }
+        
+        else{
+            ans.push_back(intervals[i]);
+        }
+       }
+       return ans;
     }
 };
