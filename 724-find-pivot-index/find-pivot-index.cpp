5c987fd8613c3dbdd8b627f65e1e6745da495cdf
bikashraj6202-hash <bikashraj6202@gmail.com>
2026-07-06 13:56:31 +0530

    Time: 0 ms (100.00%) | Memory: 37.2 MB (10.80%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.cpp
+++ 724-find-pivot-index/find-pivot-index.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> left(n, 0);
-        vector<int> right(n, 0);
-        for (int i = 1; i < n; ++i) {
-            left[i] = left[i - 1] + nums[i - 1];
+        int n=nums.size();
+      vector<int>p(n,0);
+      vector<int>s(n,0);
+      for(int i=1;i<n;i++){
+        p[i]=p[i-1]+nums[i-1];
+        s[n-1-i]=s[n-i]+nums[n-i];
+      }
+      for(int i=0;i<n;i++){
+        if(p[i]==s[i]){
+            return i;
         }
-        for (int i = n - 2; i >= 0; --i) {
-            right[i] = right[i + 1] + nums[i + 1];
-        }
-        for (int i = 0; i < n; ++i) {
-            if (left[i] == right[i]) {
-                return i;
-            }
-        }
-        return -1;
+      }
+      return -1;
     }
 };
