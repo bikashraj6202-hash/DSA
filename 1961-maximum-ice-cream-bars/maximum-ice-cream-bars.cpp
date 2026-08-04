bef03d0f39e096373454fa54de22487134e2f15e
bikashraj6202-hash <bikashraj6202@gmail.com>
2026-08-04 19:28:51 +0530

    Time: 11 ms (74.15%) | Memory: 87.8 MB (14.59%) - LeetSync

--- 1961-maximum-ice-cream-bars/maximum-ice-cream-bars.cpp
+++ 1961-maximum-ice-cream-bars/maximum-ice-cream-bars.cpp
@@ -1,11 +1,22 @@
 class Solution {
 public:
     int maxIceCream(vector<int>& costs, int coins) {
-        sort(costs.begin(),costs.end());
+        int a=*max_element(costs.begin(),costs.end());
+        vector<int>v(a+1,0);
         int n=costs.size();
-        int cnt=0;
         for(int i=0;i<n;i++){
-            coins-=costs[i];
+            v[costs[i]]++;
+        }
+        for(int i=1;i<=a;i++){
+            v[i]+=v[i-1];
+        }
+        vector<int>e(n,0);
+        for(int i=n-1;i>=0;i--){
+            e[--v[costs[i]]]=costs[i];
+        }
+      int cnt=0;
+        for(int i=0;i<n;i++){
+            coins-=e[i];
            if(coins<0){
             return cnt;
            }
