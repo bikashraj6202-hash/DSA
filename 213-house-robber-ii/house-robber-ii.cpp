a15401a0720ffbf0402176755ef18e1a40f5fd13
bikashraj6202-hash <bikashraj6202@gmail.com>
2026-08-11 09:44:00 +0530

    Time: 0 ms (100.00%) | Memory: 11 MB (19.30%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,33 +1,32 @@
 class Solution {
 public:
-    int solve(vector<int>v){
-       int idx=v.size();
-            int prev2=0,prev1=v[0];
-           for(int i=1;i<idx;i++){
-            int taken=v[i];
-            if(i>1) taken+= prev2;
-            int nottaken=0+prev1;
-            int curr=max(taken,nottaken);
-            prev2=prev1;
-            prev1=curr;
-           }
-            return prev1;
+      int solve(int idx,vector<int>&nums,vector<int>&dp){
+        if(idx==0)return nums[idx];
+        if(idx<0)return 0;
+        if(dp[idx]!=-1)return dp[idx];
+        int p=nums[idx]+solve(idx-2,nums,dp);
+        int np=0+solve(idx-1,nums,dp);
+        dp[idx]=max(p,np);
+        return dp[idx];
     }
-    
     int rob(vector<int>& nums) {
-        int n=nums.size();
-        if(n==1){
+         if(nums.size()==1){
             return nums[0];
         }
-        vector<int>v1,v2;
-        for(int i=0;i<n-1;i++){
+        vector<int>v1;
+        vector<int>v2;
+        vector<int>dp1(nums.size()-1,-1);
+         vector<int>dp2(nums.size()-1,-1);
+         for(int i=0;i<nums.size()-1;i++){
             v1.push_back(nums[i]);
-        }
-        for(int i=1;i<n;i++){
+         }
+           for(int i=1;i<nums.size();i++){
             v2.push_back(nums[i]);
-        }
-       int a=solve(v1); 
-       int b=solve(v2);
-       return max(a,b);
+         }
+          int idx=nums.size()-2;
+        int a= solve(idx,v1,dp1);
+          idx=nums.size()-2;
+        int b= solve(idx,v2,dp2);
+        return max(a,b);
     }
 };
