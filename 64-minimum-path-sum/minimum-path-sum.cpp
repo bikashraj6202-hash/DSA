c80f8bed6f1980e83f279910349c89ef115f7c46
bikashraj6202-hash <bikashraj6202@gmail.com>
2026-08-12 01:04:27 +0530

    Time: 0 ms (100.00%) | Memory: 16.4 MB (42.95%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -14,7 +14,18 @@ public:
     int minPathSum(vector<vector<int>>& g) {
         int m=g.size();
         int n=g[0].size();
-        vector<vector<int>>dp(m,vector<int>(n,-1));
-        return solve(m-1,n-1,g,dp);
+        vector<vector<int>>dp(m,vector<int>(n,0));
+
+        dp[0][0]=g[0][0];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+               if(i==0 && j==0)continue;
+               int left = 1e9; int right = 1e9;
+                if(i>0) left=g[i][j]+dp[i-1][j];
+                if(j>0)  right=g[i][j]+dp[i][j-1];
+                dp[i][j]=min(left,right);
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
