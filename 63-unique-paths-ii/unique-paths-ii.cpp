3fde8a4254c06a4cca6c9672a5f979d52274ad0a
bikashraj6202-hash <bikashraj6202@gmail.com>
2026-08-12 00:17:01 +0530

    Time: 3 ms (9.70%) | Memory: 12 MB (6.66%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -1,37 +1,19 @@
 class Solution {
 public:
-    //  int solve(int m,int n,vector<vector<int>>&dp ,vector<vector<int>>& obstacleGrid){
-    //     if(m<0 || n<0)return 0;
-    //      if(obstacleGrid[m][n]==1)return 0;
-    //     if(m==0 || n==0)return 1;
-    //     if(dp[m][n]!=-1)return dp[m][n];
-    //     int left=solve(m-1,n,dp,obstacleGrid);
-    //     int right=solve(m,n-1,dp,obstacleGrid);
-    //     dp[m][n]= left+right;
-    //     return dp[m][n];
-    // }
-    int solve(int m,int n,vector<vector<int>>&dp ,vector<vector<int>>& obstacleGrid){
-        // dp[m][n];
-        for(int i=0;i<=m;i++){
-            for(int j=0;j<=n;j++){
-                if( obstacleGrid[i][j]==1)dp[i][j]=0;
-                else if(i==0  && j==0)dp[i][j]=1;
-                else{
-                int left=0;
-                int right=0;
-                if(i>0) left=dp[i-1][j];
-                if(j>0) right=dp[i][j-1];
-                dp[i][j]=right+left;
-                }
-            }
-        }
+    
+    int solve(int m ,int n,vector<vector<int>>& ob,vector<vector<int>>&dp){
+        if(m<0 || n<0 || ob[m][n]==1)return 0;
+        if(dp[m][n]!=-1)return dp[m][n];
+        if(m==0 && n==0)return 1;
+        // int left=solve(m-1,n,ob);
+        // int right=solve(m,n-1,ob);
+        dp[m][n]=solve(m-1,n,ob,dp)+solve(m,n-1,ob,dp);
         return dp[m][n];
     }
-    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        int m=obstacleGrid.size();
-        int n=obstacleGrid[0].size();
-        
-         vector<vector<int>>dp(m,vector<int>(n,0));
-        return solve(m-1,n-1,dp,obstacleGrid);
+    int uniquePathsWithObstacles(vector<vector<int>>& ob) {
+        int m=ob.size();
+        int n=ob[0].size();
+        vector<vector<int>>dp(m,vector<int>(n,-1));
+        return solve(m-1,n-1,ob,dp);
     }
 };
